6a3b67d99a565161948ca0cae69e64d45084a275
natinha <naticamposs010607@gmail.com>
2025-09-08 18:35:22 -0300

    Etapa 4

---
+++ etapa4/src/modules/contatos/contato.service.js
@@ -0,0 +1,29 @@
+export class ContatoService {
+  
+    // Agora o Service RECEBE o repositório. Ele não o cria mais.
+    constructor(contatoRepository) {
+      this.contatoRepository = contatoRepository;
+    }
+    
+    getAllContatos() {
+        return this.contatoRepository.findAll();
+      }
+    
+      getContatoById(id) {
+        return this.contatoRepository.findById(id);
+      }
+    
+      createContato(contatoData) {
+        // No futuro, regras de negócio como "verificar email duplicado"
+        // viveriam aqui, ANTES de chamar o repositório.
+        return this.contatoRepository.create(contatoData);
+      }
+    
+      updateContato(id, contatoData) {
+        return this.contatoRepository.update(id, contatoData);
+      }
+    
+      deleteContato(id) {
+        return this.contatoRepository.remove(id);
+      }
+    }
